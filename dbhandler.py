ac47a3c750faf516d3d16ba09d555bd907696f20
josegarban <36708774+josegarban@users.noreply.github.com>
2019-03-13 11:42:27 -0400

    Split functions to compare dictionaries

--- dbhandler.py
+++ dbhandler.py
@@ -23,9 +23,9 @@ def dictfieldnames_to_tuplist(input_dict):
     primarykey_istext = True
     for outer_key in input_dict.keys():
         try:
-            if type(outer_key) is int: primary_keyistext = False
+            if type(outer_key) is int: primarykey_istext = False
         except:
-            primary_keyistext = True
+            primarykey_istext = True
             continue # At the first sight of a non-integer key, the loop will end
     
     if   primarykey_istext: output_list.append(("id", "VARCHAR(32) PRIMARY KEY"))
@@ -184,7 +184,198 @@ def fill_table(input_dict, sql_filename = "", sql_table = ""):
 
 
 ####################################################################################################
-# FUNCTIONS TO COMPARE DICTIONARIES AND TABLES
+# FUNCTIONS TO DESCRIBE AND COMPARE LISTS AND DICTIONARIES
+####################################################################################################
+
+def print_dictdescription(input_dict, printinstructions = True):
+    """
+    Input: any dictionary
+             printinstructions will let some intermediate stepts to be reported on-screen
+    Objective: count the rows in the dictionary and print it.
+    Returns: a string describing the dictionary.
+    """
+    dictdescription_string = "{0} rows found in dictionary: {1}\n".format(len(input_dict), input_dict)
+    if printinstructions == True: print(dictdescription_string)
+
+    return dictdescription_string
+
+####################################################################################################
+
+def getdictkeys(input_dict, name = "dictionary", printinstructions = True):
+    """
+    Input: any dictionary and its name (optional)
+             printinstructions will let some intermediate stepts to be reported on-screen
+    Objective: count the rows in the dictionary and print it.
+    Returns: a list containing the dictionary keys.
+    """
+
+    # Get the dictionary keys
+    dictvalues_list = [x for x in input_dict.keys()]
+    if printinstructions == True:
+        print("{0} keys found in {1}: {2}\n".format(
+            len(dictvalues_list), name, dictvalues_list))
+
+    return dictvalues_list
+
+####################################################################################################
+
+def compare_twolists(list1, list2, list1name = "", list2name = "", printinstructions = True):
+    """
+    Input: two dictionaries and their names (optional strings),
+            printinstructions will let some intermediate stepts to be reported on-screen.
+    Objective: comparing two lists.
+    Output: tuple of the form (are values the same?, which values are different).
+    """
+
+    # Get what is in list1 but not in list2
+    list1_notin_list2 = [x for x in list1 if x not in list2]
+    if printinstructions == True:
+        print("{0} keys found in {2} but not in {3}: {1}\n".format(
+            len(list1_notin_list2), list1_notin_list2, list1name, list2name))    
+    
+    # Get what is in list2 but not in list1
+    list2_notin_list1 = [x for x in list2 if x not in list1]
+    if printinstructions == True:
+        print("{0} keys found in {3} but not in {2}: {1}\n".format(
+            len(list2_notin_list1), list2_notin_list1, list1name, list2name))    
+
+    if list1_notin_list2 == [] and list2_notin_list1 == []: same = True
+    else: same = False
+
+    return (same, list1_notin_list2, list2_notin_list1)
+
+####################################################################################################
+
+def compare_twodictkeys(dict1, dict2, dict1name = "", dict2name = "", printinstructions = True):
+    """
+    Input: two dictionaries and their names (optional strings),
+            printinstructions will let some intermediate stepts to be reported on-screen.
+    Objective: comparing two dictionary keys.
+    Output: tuple of the form (are values the same?, which values are different).
+    """
+    # Get lists with dictionary keys
+    dict1keys = getdictkeys(dict1, dict1name, printinstructions)
+    dict2keys = getdictkeys(dict2, dict2name, printinstructions)
+    
+    # Generate names for the dictionary key names to appear on screen
+    if dict1name == "": dict1keysname = "dictionary keys"
+    else: dict1keysname = dict1name + " keys"
+    
+    if dict2name == "": dict2keysname = "dictionary keys"
+    else: dict2keysname = dict2name + " keys"
+    
+    dictkey_comparison = compare_twolists(dict1keys, 
+                                          dict2keys,
+                                          dict1keysname,
+                                          dict2keysname,
+                                          printinstructions)
+    
+    dict1keys_notin_dict2keys = dictkey_comparison[1]
+    dict2keys_notin_dict1keys = dictkey_comparison[2]
+
+    # Get what is in one dictionary but not in the other
+    dict1_notin_dict2 = {x : dict1[x] for x in dict1keys_notin_dict2keys}
+    if printinstructions == True:
+        print("{0} rows found in {1} but not in {2}:".format(
+            len(dict1_notin_dict2), dict1name, dict2name))
+        pprint.pprint(dict1_notin_dict2)
+        print("")
+        
+    dict2_notin_dict1 = {x : dict2[x] for x in dict2keys_notin_dict1keys}
+    if printinstructions == True:
+        print("{0} rows found in {2} but not in {1}:".format(
+            len(dict2_notin_dict1), dict1name, dict2name))
+        pprint.pprint(dict2_notin_dict1)    
+        print("")
+        
+    if dict1_notin_dict2 == {} and dict2_notin_dict1 == {}: same = True
+    else: same = False
+    
+    return (same, dict1_notin_dict2, dict2_notin_dict1)
+
+
+    
+####################################################################################################
+# FUNCTIONS TO COMPARE DATA IN A DICTIONARY AND DATA IN A TABLE DATABASE
+####################################################################################################
+
+def get_alldbkeys(sql_filename = "",
+                  sql_table = "",
+                  dbkey_column = "id",
+                  printinstructions = True):
+    """
+    Inputs: filename,
+            table that will opened,
+            column in the database table is assumed to be called "id", (can be changed)
+            printinstructions will let some intermediate stepts to be reported on-screen
+    Objective: get all keys in a database table.
+    Outputs: list with keys in the database table.
+    """
+
+    # Open the database and get the table name if none has been set
+    my_connector  = create_connector(sql_filename)
+    my_cursor     = my_connector.cursor()
+    if sql_table  == "": sql_table = input("Insert table name:")
+
+    # Build the instruction to be executed to get the keys in the database
+    instruction   = """SELECT {0} FROM {1}""".format(dbkey_column, sql_table)                 
+
+    # Execute the instruction to get a tuple for each table row
+    my_cursor.execute(instruction)
+    dbvalues_tups = my_cursor.fetchall()
+    
+    # Create and report the output
+    output_list   = [x[0] for x in dbvalues_tups]
+    if printinstructions == True:
+        print("Instruction executed: {0} in {1}.\n{2} keys found in database: {3}\n".format(
+            instruction, sql_filename, len(output_list), output_list))    
+
+    return output_list
+
+####################################################################################################
+
+def get_alldbrows(sql_filename = "",
+                  sql_table = "",
+                  dbkey_column = "id",
+                  printinstructions = True):
+    """
+    Inputs: filename,
+            table that will opened,
+            column in the database table is assumed to be called "id", (can be changed)
+            printinstructions will let some intermediate stepts to be reported on-screen
+    Objective: get all values in a database table.
+    Outputs: nested dictionary of the form { id value: {"column name": column value}...}.
+    """
+    # Open the database and get the table name if none has been set
+    my_connector  = create_connector(sql_filename)
+    my_cursor     = my_connector.cursor()
+    if sql_table  == "": sql_table = input("Insert table name:")
+
+    # Build the instruction to be executed to get the keys in the database
+    instruction   = """SELECT * FROM {0}""".format(sql_table)                 
+
+    # Execute the instruction
+    dbvalues_tups = [x for x in my_cursor.execute(instruction)]
+
+    #Find the index of the key (probably "id") in the database table
+    db_colnames   = [x[0] for x in my_cursor.description]
+    keycol_index  = db_colnames.index(dbkey_column)
+    
+    # Convert the tuples from the database into a dictionary of the form {"id" {"values": values}}
+    output_dict = {}
+    for tup in dbvalues_tups:
+        output_dict[tup[keycol_index]] = {}
+        for col in db_colnames:
+            if col != dbkey_column: # key column (probably "id") is outside the inner dictionary
+                output_dict[tup[keycol_index]][col] = tup[db_colnames.index(col)]
+    
+    # Report the output
+    if printinstructions == True:
+        print("Instruction executed: {0} in {1}.\n{2} rows found in database: {3}\n".format(
+            instruction, sql_filename, len(output_dict), output_dict))    
+    
+    return output_dict
+    
 ####################################################################################################
 
 def compare_keysonly(input_dict,
@@ -193,51 +384,23 @@ def compare_keysonly(input_dict,
                      dbkey_column = "id",
                      printinstructions = True):
     """
-    Inputs: filename, table that will opened, and a dictionary.
-            The column in the database table is assumed to be called "id",
-            but this can be changed at the input level.
-            Intermediate steps are reported or not, according to printinstructions.
+    Inputs: dictionary that will be compared with the database,
+            filename,
+            table that will opened,
+            column in the database table is assumed to be called "id", (can be changed)
+            printinstructions will let some intermediate stepts to be reported on-screen
     Objective: compare keys in the dictionary against values in the table.
-    Outputs: tup of the form (are values the same?, which values are different).
+    Outputs: tuple of the form (are values the same?, which values are different).
     """
 
-    # Open the database and get the table name if none has been set
-    my_connector = create_connector(sql_filename)
-    my_cursor    = my_connector.cursor()
-    if sql_table == "": sql_table = input("Insert table name:")
-
-    # Build the instruction to be executed to get the keys in the database
-    instruction    = """SELECT id FROM {1}""".format(
-        dbkey_column, sql_table)                 
-
-    # Execute the instruction
-    my_cursor.execute(instruction)
-    dbvalues_tups = my_cursor.fetchall()
-    dbvalues_list = [x[0] for x in dbvalues_tups]
-    if printinstructions == True:
-        print("Instruction executed: {0} in {1}.\n{2} keys found in database: {3}\n".format(
-            instruction, sql_filename, len(dbvalues_list), dbvalues_list))    
-
-    # Get the dictionary keys
-    dictvalues_list = [x for x in input_dict.keys()]
-    if printinstructions == True:
-        print("{0} keys found in dictionary: {1}\n".format(
-            len(dictvalues_list), dictvalues_list))    
+    # Get the keys in the dictionary and in the database table
+    dictvalues_list = getdictkeys(input_dict)        
+    dbvalues_list = get_alldbkeys(sql_filename, sql_table, dbkey_column, printinstructions)
 
     # Get what is in one list but not in the other
-    dictvalues_not_indb = [x for x in dictvalues_list if x not in dbvalues_list]
-    if printinstructions == True:
-        print("{0} keys found in dictionary but not in database: {1}\n".format(
-            len(dictvalues_not_indb), dictvalues_not_indb))    
-    dbvalues_not_indict = [x for x in dbvalues_list if x not in dictvalues_list]
-    if printinstructions == True:
-        print("{0} keys found in database but not in dictionary: {1}\n".format(
-            len(dbvalues_not_indict), dbvalues_not_indict))    
-
-    if dictvalues_not_indb == [] and dbvalues_not_indict == []: same = True
-    else: same = False
+    actual_comparison = compare_twolists(dictvalues_list, dbvalues_list, "dictionary", "database", printinstructions)
     
-    return (same, dictvalues_not_indb, dbvalues_not_indict)
+    return actual_comparison
 
 ####################################################################################################
 
@@ -247,71 +410,52 @@ def compare_keysfull(input_dict,
                      dbkey_column = "id",
                      printinstructions = True):
     """
-    Inputs: filename, table that will opened, and a dictionary.
-            The column in the database table is assumed to be called "id",
-            but this can be changed at the input level.
-            Intermediate steps are reported or not, according to printinstructions.
+    Inputs: dictionary that will be compared with the database,
+            filename,
+            table that will opened,
+            column in the database table is assumed to be called "id", (can be changed)
+            printinstructions will let some intermediate stepts to be reported on-screen
     Objective: compare keys in the dictionary against values in the table.
             However, different values besides "id" are not compared.
     Outputs: tup of the form (are values the same?, which values are different).
     """
 
-    # Open the database and get the table name if none has been set
-    my_connector = create_connector(sql_filename)
-    my_cursor    = my_connector.cursor()
-    if sql_table == "": sql_table = input("Insert table name:")
+    # Convert the table into a dictionary to compare with input_dict
+    db_dict = get_alldbrows(sql_filename, sql_table, dbkey_column, printinstructions)
 
-    # Build the instruction to be executed to get the keys in the database
-    instruction    = """SELECT * FROM {0}""".format(
-        sql_table)                 
+    # Describe the dictionary
+    print_dictdescription(input_dict, printinstructions)    
 
-    # Execute the instruction
-    dbvalues_tups = [x for x in my_cursor.execute(instruction)]
-    db_colnames   = [x[0] for x in my_cursor.description]
-
-    #Find the index of the key (probably "id") in the database table
-    keycol_index  = db_colnames.index(dbkey_column)
+    # Compare the dictionary
+    actual_comparison = compare_twodictkeys(input_dict, db_dict, "input dictionary", "database dictionary")
     
-    # Convert the tuples from the database into a dictionary of the form {"id" {"values": values}}
-    db_dict = {}
-    for tup in dbvalues_tups:
-        db_dict[tup[keycol_index]] = {}
-        for col in db_colnames:
-            if col != dbkey_column:
-                # key column (probably "id") is outside the inner dictionary
-                db_dict[tup[keycol_index]][col] = tup[db_colnames.index(col)]
+    return actual_comparison
+
+####################################################################################################
+
+def compare_rowsfull(input_dict,
+                     sql_filename = "",
+                     sql_table = "",
+                     dbkey_column = "id",
+                     printinstructions = True):
+    """
+    Inputs: dictionary that will be compared with the database,
+            filename,
+            table that will opened,
+            column in the database table is assumed to be called "id", (can be changed)
+            printinstructions will let some intermediate stepts to be reported on-screen
+    Objective: compare full rows in the dictionary against values in the table.
+            Values other than "id" are compared.
+    Outputs: tup of the form (are rows the same?, which rows are different).
+    """
+
+    # Convert the table into a dictionary to compare with input_dict
+    db_dict = get_alldbrows(sql_filename, sql_table, dbkey_column, printinstructions)
+
+    # Describe the dictionary
+    print_dictdescription(input_dict, printinstructions)    
+
     
-    if printinstructions == True:
-        print("Instruction executed: {0} in {1}.\n{2} rows found in database: {3}\n".format(
-            instruction, sql_filename, len(db_dict), db_dict))    
-
-    # Get the dictionary keys
-    if printinstructions == True:
-        print("{0} rows found in dictionary: {1}\n".format(
-            len(input_dict), input_dict))    
-
-    # Get what is in one list but not in the other
-    key_comparison = compare_keysonly(input_dict, sql_filename, sql_table, dbkey_column, False)
-    dictkeys_not_indb = key_comparison[1]
-    dbkeys_not_indict = key_comparison[2]
-
-    # Get what is in one dictionary but not in the other
-    dictvalues_not_indb = {x : input_dict[x] for x in dictkeys_not_indb}
-    if printinstructions == True:
-        print("{0} rows found in dictionary but not in database:".format(
-            len(dictvalues_not_indb)))
-        pprint.pprint(dictvalues_not_indb)
-        print("")
-    dbvalues_not_indict = {x : db_dict[x] for x in dbkeys_not_indict}
-    
-    if printinstructions == True:
-        print("{0} rows found in database but not in dictionary:".format(
-            len(dbvalues_not_indict)))
-        pprint.pprint(dbvalues_not_indict)    
-        print("")
-        
-    if dictvalues_not_indb == {} and dbvalues_not_indict == {}: same = True
-    else: same = False
     
     return (same, dictvalues_not_indb, dbvalues_not_indict)
 
